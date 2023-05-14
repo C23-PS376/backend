c27a0e070041b6efff661f660e75136aa63f5cc5
martuafernando <fernandosibarani45@gmail.com>
2023-05-14 10:48:01 +0700

    fix user CRUD

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,11 +1,11 @@
 import { HttpException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { validate } from 'class-validator';
+import { DeleteResult, Repository } from 'typeorm';
+import * as argon2 from 'argon2';
 
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { User } from './entities/user.entity';
-import { Repository } from 'typeorm';
 
 @Injectable()
 export class UserService {
@@ -24,14 +24,6 @@ export class UserService {
     return await this.userRepository.save(user)
   }
 
-  findOneById(id: number): Promise<User|null> {
-    return this.userRepository.findOneBy({id});
-  }
-
-  findOneByEmail(email: string): Promise<User|null> {
-    return this.userRepository.findOneBy({email});
-  }
-
   async update(id: number, updateUserDto: UpdateUserDto): Promise<User> {
     const existingUser = await this.findOneById(id)
     if (!existingUser) throw new HttpException('User didn\'t exists', 400)
@@ -44,12 +36,25 @@ export class UserService {
     return await this.userRepository.save(updatedUser)
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async remove(id: number): Promise<DeleteResult> {
+    const existingUser = await this.findOneById(id)
+    if (!existingUser) throw new HttpException('User didn\'t exists', 400)
+
+    return this.userRepository.delete({id})
   }
 
-  validate(email: string) {
-    const user = this.findOneByEmail(email)
-    if (user) throw new HttpException('Email already exists', 400)
+  async validate(email: string, password: string): Promise<boolean> {
+    const user = await this.findOneByEmail(email)
+    if (!user) throw new HttpException('User didn\'t exists', 400)
+
+    return await argon2.verify(user.password, password)
+  }
+
+  findOneById(id: number): Promise<User|null> {
+    return this.userRepository.findOneBy({id});
+  }
+
+  findOneByEmail(email: string): Promise<User|null> {
+    return this.userRepository.findOneBy({email});
   }
 }
