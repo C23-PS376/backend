5f3f59bef3462fc266caac86a87aacb641e46d44
ifs20035-itdel <ifs20035@students.del.ac.id>
2023-06-09 18:17:00 +0700

    update, remove, listComment, fix

--- dist/src/comment/comment.service.js
+++ dist/src/comment/comment.service.js
@@ -29,25 +29,155 @@ const typeorm_1 = require("@nestjs/typeorm");
 const storage_service_1 = require("../storage/storage.service");
 const typeorm_2 = require("typeorm");
 const comment_entity_1 = require("./entities/comment.entity");
+const user_entity_1 = require("../user/entities/user.entity");
+const thread_entity_1 = require("../thread/entities/thread.entity");
+const user_service_1 = require("../user/user.service");
+const thread_service_1 = require("../thread/thread.service");
+const fs = require("fs");
+const tmp = require("tmp");
+const get_audio_duration_1 = require("get-audio-duration");
 let CommentService = class CommentService {
-    constructor(commentRepository, storageService) {
+    constructor(commentRepository, userRepository, threadRepository, userService, threadService, storageService) {
         this.commentRepository = commentRepository;
+        this.userRepository = userRepository;
+        this.threadRepository = threadRepository;
+        this.userService = userService;
+        this.threadService = threadService;
         this.storageService = storageService;
     }
     async create(createCommentDto, userId, threadId) {
         const comment = new comment_entity_1.Comment();
         const { audio } = createCommentDto, createdData = __rest(createCommentDto, ["audio"]);
+        if (!this.threadService.findOneById(+threadId))
+            throw new common_1.HttpException("Thread doesn't exists", 400);
         const newAudioPath = audio
-            ? await this.storageService.save('commment/audio-', audio)
+            ? await this.storageService.save('comments/audio-', audio)
             : undefined;
-        Object.assign(comment, Object.assign({ user: userId, thread: threadId, audio: newAudioPath }, createdData));
+        const newAudioLength = audio
+            ? await this.getAudioDuration(audio.buffer)
+            : undefined;
+        Object.assign(comment, Object.assign({ user: userId, thread: threadId, audio: newAudioPath, audio_length: newAudioLength }, createdData));
+        const user = await this.userService.findOneById(+userId);
+        const thread = await this.threadService.findOneById(+threadId);
+        user.comments_count = (+user.threads_count + 1).toString();
+        thread.comments_count = (+thread.comments_count + 1).toString();
         return await this.commentRepository.save(comment);
+    }
+    async update(commentId, threadId, updateCommentDto, userId) {
+        const existingComment = await this.commentRepository.findOne({
+            where: {
+                id: commentId,
+                thread: { id: threadId },
+            },
+            relations: {
+                user: true,
+                thread: true,
+            },
+        });
+        if (!existingComment)
+            throw new common_1.HttpException("Comment didn't exists", 400);
+        if (existingComment.user.id !== userId && existingComment.thread.id !== threadId)
+            throw new common_1.ForbiddenException();
+        const { audio } = updateCommentDto, updatedData = __rest(updateCommentDto, ["audio"]);
+        const oldAudioPath = this.storageService.getFilenameFromPath(existingComment.audio);
+        const newAudioPath = audio
+            ? await this.storageService.save('comments/audio-', audio)
+            : undefined;
+        Object.assign(existingComment, Object.assign({ user: userId, thread: threadId, audio: newAudioPath }, updatedData));
+        if (audio)
+            this.storageService.removeFileIfExists(oldAudioPath);
+        return await this.commentRepository.save(existingComment);
+    }
+    async remove(id, threadId, userId) {
+        const existingComment = await this.commentRepository.findOne({
+            where: {
+                id,
+                thread: { id: threadId },
+            },
+            relations: {
+                user: true,
+            },
+        });
+        if (!existingComment)
+            throw new common_1.HttpException("Comment didn't exists", 400);
+        if (existingComment.user.id !== userId)
+            throw new common_1.ForbiddenException();
+        const oldAudioPath = this.storageService.getFilenameFromPath(existingComment.audio);
+        this.storageService.removeFileIfExists(oldAudioPath);
+        return this.commentRepository.delete({ id });
+    }
+    async findOneById(commentId, threadId) {
+        const comment = await this.commentRepository.findOne({
+            where: {
+                id: +commentId,
+                thread: { id: threadId },
+            },
+            select: {
+                user: {
+                    name: true,
+                },
+                thread: {
+                    id: true,
+                }
+            }
+        });
+        if (!comment)
+            throw new common_1.HttpException("Comment doesn't exists", 400);
+        return comment;
+    }
+    async findAll(threadId, size) {
+        const comments = await this.commentRepository.find({
+            where: {
+                thread: { id: threadId }
+            },
+            order: {
+                created_at: 'DESC'
+            },
+            take: size,
+            relations: [
+                'user'
+            ]
+        });
+        const data = comments.map(comment => ({
+            id: comment.id,
+            text: comment.text,
+            audio: comment.audio,
+            audio_length: comment.audio_length,
+            created_at: comment.created_at,
+            updated_at: comment.updated_at,
+            user: {
+                name: comment.user.name,
+            },
+        }));
+        return data;
+    }
+    getAudioDuration(audioBuffer) {
+        return new Promise((resolve, reject) => {
+            tmp.file(function (err, path, fd, cleanup) {
+                if (err)
+                    throw err;
+                fs.appendFile(path, audioBuffer, function (err) {
+                    if (err)
+                        reject(err);
+                    (0, get_audio_duration_1.default)(path).then((duration) => {
+                        cleanup();
+                        resolve(duration);
+                    });
+                });
+            });
+        });
     }
 };
 CommentService = __decorate([
     (0, common_1.Injectable)(),
     __param(0, (0, typeorm_1.InjectRepository)(comment_entity_1.Comment)),
+    __param(1, (0, typeorm_1.InjectRepository)(user_entity_1.User)),
+    __param(2, (0, typeorm_1.InjectRepository)(thread_entity_1.Thread)),
     __metadata("design:paramtypes", [typeorm_2.Repository,
+        typeorm_2.Repository,
+        typeorm_2.Repository,
+        user_service_1.UserService,
+        thread_service_1.ThreadService,
         storage_service_1.StorageService])
 ], CommentService);
 exports.CommentService = CommentService;
--- dist/src/comment/dto/update-comment.dto.js
+++ dist/src/comment/dto/update-comment.dto.js
@@ -1,9 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.UpdateCommentto = void 0;
+exports.UpdateCommentDto = void 0;
 const mapped_types_1 = require("@nestjs/mapped-types");
 const create_comment_dto_1 = require("./create-comment.dto");
-class UpdateCommentto extends (0, mapped_types_1.PartialType)(create_comment_dto_1.CreateCommentDto) {
+class UpdateCommentDto extends (0, mapped_types_1.PartialType)(create_comment_dto_1.CreateCommentDto) {
 }
-exports.UpdateCommentto = UpdateCommentto;
+exports.UpdateCommentDto = UpdateCommentDto;
 //# sourceMappingURL=update-comment.dto.js.map
